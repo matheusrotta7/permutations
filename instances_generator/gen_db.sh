51530b9f30af91975ca44d79c7c5255a59e40514
Gabriel Siqueira <gabriel.gabrielhs@gmail.com>
2021-07-12 17:35:33 -0300

    Atualização das bases de dados

--- instances_generator/gen_db.sh
+++ instances_generator/gen_db.sh
@@ -1,12 +1,16 @@
 k=1000
-op=25
 
 mkdir database
-for type in {Rev,Trans,Swap,Insertion}
-do
+# for type in {Rev,Trans}
+# do
+type=RevTrans
     mkdir database/${type}
-    for n in `seq 50 50 500`
+    for op in {25,50,75,100}
     do
-        stack exec DB -- -k $k -n $n -r $op -t $type -o database/${type}/${type}_R${op}_N${n}.txt
+        mkdir database/${type}/$op
+        for n in `seq 50 50 500`
+        do
+            stack exec DB -- -k $k -n $n -r $op -t $type -p 50 -o database/${type}/$op/${type}_R${op}_N${n}.txt
+        done
     done
-done
+# done
